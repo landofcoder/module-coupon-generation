fb7d7c6b8cab47ba68222b20492cc7d262abcd93
Misha <landofcoder@gmail.com>
2022-11-24 16:29:58 +0700

    refactor

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * LandofCoder
- * 
+ *
  * NOTICE OF LICENSE
- * 
+ *
  * This source file is subject to the venustheme.com license that is
  * available through the world-wide-web at this URL:
  * http://venustheme.com/license
- * 
+ *
  * DISCLAIMER
- * 
+ *
  * Do not edit or add to this file if you wish to upgrade this extension to newer
  * version in the future.
- * 
+ *
  * @category   LandofCoder
  * @package    Lof_CouponCode
  * @copyright  Copyright (c) 2019 Landofcoder (http://www.landofcoder.com/)
@@ -125,7 +125,6 @@ class InstallSchema implements InstallSchemaInterface
             \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
             null,
             ['identity' => true, 'nullable' => false, 'primary' => true],
-
             'Coupon Code ID'
         )->addColumn(
             'alias',
